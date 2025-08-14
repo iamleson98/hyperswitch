8e5eccbe73281288b208ebbe605965cf9c08223d
Malay Awasthi <malay.awasthi@juspay.in>
2025-08-14 14:12:48 +0530

    feat/gpay-encryptedFlow

--- crates/router/src/core/payments/transformers.rs
+++ crates/router/src/core/payments/transformers.rs
@@ -1,5 +1,5 @@
 use std::{fmt::Debug, marker::PhantomData, str::FromStr};
-
+use masking::PeekInterface;
 use api_models::payments::{
     Address, ConnectorMandateReferenceId, CustomerDetails, CustomerDetailsResponse, FrmMessage,
     MandateIds, RequestSurchargeDetails,
@@ -1264,6 +1264,14 @@ where
     let l2_l3_data = state.conf.l2_l3_data_config.enabled.then(|| {
         let shipping_address = unified_address.get_shipping();
         let billing_address = unified_address.get_payment_billing();
+        let merchant_details = merchant_context.get_merchant_account().get_merchant_details();
+        let merchant_tax_registration_id = merchant_details.as_ref().and_then(|details| {
+            details
+                .get_inner()
+                .peek()
+                .get("merchant_tax_registration_id")
+                .and_then(|id| id.as_str().map(|s| Secret::new(s.to_string())))
+        });
 
         types::L2L3Data {
             order_date: payment_data.payment_intent.order_date,
@@ -1306,6 +1314,7 @@ where
                 .as_ref()
                 .and_then(|addr| addr.address.as_ref())
                 .and_then(|details| details.city.clone()),
+            merchant_tax_registration_id,
         }
     });
     crate::logger::debug!("unified address details {:?}", unified_address);
